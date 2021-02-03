1045656709bb7e0550a1503d82d5d537725c9871
mayukuno <kunomayu1006@gmail.com>
2021-02-03 23:03:10 +0900

    Update tasks.rb

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :task do
     taskname { 'rubyの勉強をする' }
-    description { 'ドットインストールで講座を受ける'}
+    description { 'ドットインストールを受ける'}
     created_at { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now) }
     association :user
   end
