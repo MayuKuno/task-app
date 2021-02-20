2cc8ccaf0edf18b47b1954139e3b282a4bfe82a6
mayukuno <kunomayu1006@gmail.com>
2021-02-20 20:08:40 +0900

    preview etc

--- app/assets/javascripts/imageUpload.js
+++ app/assets/javascripts/imageUpload.js
@@ -1,15 +1,15 @@
 $(window).on('load', function(){
 
     function readURL(input) {
-
-        if (input.files && input.files[0]) {
+      if (input.files && input.files[0]) {
         var reader = new FileReader();
         reader.onload = function (e) {
-
           $('#img_prev').attr('src', e.target.result);
+
         }
+
         reader.readAsDataURL(input.files[0]);
-        }
+      }
     }
     $(".file").change(function(){
       readURL(this);
