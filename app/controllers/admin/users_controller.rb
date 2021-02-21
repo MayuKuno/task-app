97755f88dfe94c6acba981b660352931efd1ee67
mayukuno <kunomayu1006@gmail.com>
2021-02-21 18:57:14 +0900

    integrate admin new to user new(edit)

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -11,42 +11,38 @@ class Admin::UsersController < ApplicationController
   end
 
 
-  def new
-    @user = User.new
-  end
+  # def new
+  #   @user = User.new
+  # end
 
-  def create
+  # def create
+  #   @user = User.new(user_params)
+  #   if @user.save
+  #     if current_user.admin?
+  #       redirect_to admin_users_path
+  #     else
+  #       log_in(@user)
+  #         # メールを送りたい時だけ
+  #         # UserNotifierMailer.send_signup_email(@user).deliver
+  #       redirect_to root_path
+  #     end
+  #   else
+  #     render :new
+  #   end
+  # end
 
-    @user = User.new(user_params)
+  # def edit
+  #   @user = User.find(params[:id])
+  # end
 
-    if @user.save
-      if current_user.admin?
-        redirect_to admin_users_path
-      else
-        log_in(@user)
-          # メールを送りたい時だけ
-          # UserNotifierMailer.send_signup_email(@user).deliver
-        redirect_to root_path
-      end
-
-
-    else
-      render :new
-    end
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      redirect_to admin_user_url(@user), notice:"ユーザー「#{@user.username}」を更新しました"
-    else
-      render :edit
-    end
-  end
+  # def update
+  #   @user = User.find(params[:id])
+  #   if @user.update(user_params)
+  #     redirect_to user_path(@user), notice:"ユーザー「#{@user.username}」を更新しました"
+  #   else
+  #     render :edit
+  #   end
+  # end
 
   def destroy
     @user = User.find(params[:id])
@@ -65,9 +61,9 @@ class Admin::UsersController < ApplicationController
 
   private
 
-  def user_params
-    params.require(:user).permit(:username, :email, :image, :admin, :password, :password_confirmation)
-  end
+  # def user_params
+  #   params.require(:user).permit(:username, :email, :image, :admin, :password, :password_confirmation)
+  # end
 
   def require_admin
     redirect_to root_url unless current_user.admin?
