934c130899efafc0ef4710a40cfea5a75175e9ad
mayukuno <kunomayu1006@gmail.com>
2021-03-04 20:17:43 +0900

    file upload validation
     済）・margin-bottomがない
     済）・select fileじゃなくimageに変更
     済）・image以外がアップロードされてしまう

--- app/models/task.rb
+++ app/models/task.rb
@@ -9,6 +9,7 @@ class Task < ApplicationRecord
   has_many :task_labels, dependent: :destroy
   has_many :labels, through: :task_labels
   has_one_attached :image
+  validate :avatar_type
 
   enum priority: { low: 0, middle: 1, high: 2 }
   enum status: { Waiting: 0, Working: 1, Completed: 2 }
@@ -45,6 +46,14 @@ class Task < ApplicationRecord
         task.attributes = row.to_hash.slice(*csv_attributes)
         task.save!
       end
+    end
+  end
+
+  private
+  def avatar_type
+    if !image.blob.content_type.in?(%('image/jpeg image/png'))
+      image.purge
+      errors.add(:image, 'はjpegまたはpng形式でアップロードしてください')
     end
   end
 
